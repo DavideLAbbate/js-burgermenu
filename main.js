65b84e92436b0d2bca9dfde0fa74a3f7653535f8
Davide LAbbate <labbate.davide98@gmail.com>
2022-05-26 10:47:18 +0200

    fixed all

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let menuMobile = document.querySelector(".menu-mobile");
-let element = document.getElementById("menu");
+let element = document.querySelector(".menu");
+let closeMenu = document.querySelector(".close")
 
 function toggleMenu (){
     menuMobile.classList.add("active")
+    element.classList.add("disable")
+    closeMenu.classList.add("able")
+}
+
+function close (){
+    menuMobile.classList.remove("active")
+    closeMenu.classList.remove("able")
+    element.classList.remove("disable")
 }
 
 element.addEventListener('click', toggleMenu);
+
+closeMenu.addEventListener('click', close)
